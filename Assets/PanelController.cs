c07f3164c1922a917a026f3b459094711f1ca13f
Terry Johnson <john24tl@dukes.jmu.edu>
2023-12-08 17:42:17 -0500

    Update PanelController.cs

--- Assets/PanelController.cs
+++ Assets/PanelController.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -12,7 +11,7 @@ public class PanelController : MonoBehaviour
     public int limit = 4;
 
     private string text = "";
-    
+
     // Update is called once per frame
     void Update()
     {
@@ -29,32 +28,50 @@ public class PanelController : MonoBehaviour
         text = "";
     }
 
-   public void EnterText(string t)
-{
-    if (limitLength)
+    public void EnterText(string t)
     {
-        text += t;
-
-        // Debug log to trace entering text and current text length
-        Debug.Log($"Text Entered: {t}, Current Text: {text}, Current Length: {text.Length}");
-
-        if (text.Length == limit)
+        if (limitLength)
         {
-            // Check if the entered code is correct
-            bool isCorrect = CheckCorrectCode(text);
+            text += t;
 
-            // Display "Correct" or "Wrong" on the panel
-            Clear();
-            EnterText(isCorrect ? "Correct" : "Wrong");
+            // Debug log to trace entering text and current text length
+            Debug.Log($"Text Entered: {t}, Current Text: {text}, Current Length: {text.Length}");
 
-            // Start a coroutine to clear the panel after 1.5 seconds
-            StartCoroutine(ClearPanelAfterDelay(1.5f));
+            if (text.Length == limit)
+            {
+                // Check if the entered code is correct
+                bool isCorrect = CheckCorrectCode(text);
+
+                // Display "Correct" or "Wrong" on the panel
+                Clear();
+                EnterText(isCorrect ? "Correct" : "Wrong");
+
+                // Start a coroutine to clear the panel after 1.5 seconds
+                StartCoroutine(ClearPanelAfterDelay(1.5f));
+            }
+        }
+        else
+        {
+            text += t;
         }
     }
-    else
-    {
-        text += t;
-    }
-}
 
+    // Method to check if the entered code is correct (modify this based on your logic)
+    private bool CheckCorrectCode(string code)
+    {
+        // Replace this with your actual logic to check if the entered code is correct
+        return code.Equals("4234");
+    }
+
+    // Coroutine method to clear the panel after a delay
+    private IEnumerator ClearPanelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        // Debug log to indicate clearing the panel after a delay
+        Debug.Log("Clearing Panel after Delay");
+
+        // Clear the panel after the delay
+        Clear();
+    }
 }
