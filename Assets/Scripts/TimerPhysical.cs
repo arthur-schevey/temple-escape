7c9167ef18876b310802eddf3127daf129bc13bf
Julian <102552495+Guerreja@users.noreply.github.com>
2023-12-09 13:36:30 -0500

    Added Oculus Hands to Assests. Added timer and hands to BallRoom.

---
+++ Assets/Scripts/TimerPhysical.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class TimerPhysical : MonoBehaviour
+{
+    public int time;
+    public TextMeshPro timer;
+    float fps = 1f;
+    float curTime;
+
+    void Start()
+    {
+        curTime = 0f;
+        SetCountText();
+    }
+
+
+    string toTimeString(int time)
+    {
+        string ret = ":";
+        if (time % 60 < 10)
+        {
+            ret = ret + "0";
+        }
+        return (time / 60).ToString() + ret + (time % 60).ToString();
+    }
+
+    void SetCountText()
+    {
+        timer.text = toTimeString(time);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        curTime += Time.deltaTime;
+        Debug.Log(curTime);
+        Debug.Log(time);
+        if (curTime > fps)
+        {
+            if (time > 0)
+            {
+                time -= 1;
+                SetCountText();
+            }
+            else
+            {
+                SceneManager.LoadScene(0);
+            }
+            
+            curTime = 0f;
+        }
+    }
+
+}
